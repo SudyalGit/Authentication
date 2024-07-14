926783b34f422fdf450dfd477e69f26b6818cbec
Sudhanshu <sudyal101@gmail.com>
2024-07-14 17:16:16 +0530

    setup project

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Authentication</title>
+
+    <link rel="stylesheet" href="./assets/css/index.css">
+</head>
+<body>
+    Authentication Home Page
+    <br>
+    <a href="./login.php">login</a>
+    <br>
+    <a href="./register.php">register</a>
+    <br>
+    <a href="./forgot-password.php">forgot password</a>
+    <br>
+    <a href="./welcome.php">welcome</a>
+</body>
+</html>
